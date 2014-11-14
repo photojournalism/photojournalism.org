a202f388d0884717b9a6da6b04d34bcc9bfba8f0
Casey Scarborough <caseyscarborough@gmail.com>
2014-11-14 15:52:13 -0500

    Hide 2014 contest winners until results are released.

--- config.rb
+++ config.rb
@@ -51,6 +51,9 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
+ignore 'images/2014/contest-winners/*'
+ignore 'contest/winners/2014/*'
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
