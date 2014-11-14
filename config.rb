2a0069ea126e32939955560651557ffca4a4db66
Casey Scarborough <caseyscarborough@gmail.com>
2014-11-14 17:52:55 -0500

    Release first round of winners for 2014 Contest.

--- config.rb
+++ config.rb
@@ -51,8 +51,12 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
-ignore 'images/2014/contest-winners/*'
-ignore 'contest/winners/2014/*'
+ignore 'images/2014/contest-winners/best-in-show/*'
+ignore 'images/2014/contest-winners/best-portfolio/*'
+ignore 'images/2014/contest-winners/rich-mahan-best-student-portfolio/*'
+ignore 'contest/winners/2014/best-in-show/*'
+ignore 'contest/winners/2014/best-portfolio/*'
+ignore 'contest/winners/2014/rich-mahan-best-student-portfolio/*'
 
 # Build-specific configuration
 configure :build do
